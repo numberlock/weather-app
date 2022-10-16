7c4dd3920aa7114f5af68728614afb514a4b5346
numberlock <numberlock@gmail.com>
2022-10-16 09:44:00 +0200

    fixed http to https

--- src/modules/geoCodingAPI.js
+++ src/modules/geoCodingAPI.js
@@ -1,7 +1,7 @@
 export default async function cordinatesConverter(input) {
   try {
     const geoCoding = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=1bdf0cbef1c66e6d7ba6d40dab697a5b`,
+      `https://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=1bdf0cbef1c66e6d7ba6d40dab697a5b`,
       { mode: "cors" }
     );
     const data = await geoCoding.json();
